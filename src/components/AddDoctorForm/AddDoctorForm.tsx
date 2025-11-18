0c5b55ed0e7dd8eda9f8b9f3d65a9e6920f765bb
Linda Hrdinová <hrlin@seznam.cz>
2025-11-18 16:10:03 +0100

    Form error & requiered detail

--- src/components/AddDoctorForm/AddDoctorForm.tsx
+++ src/components/AddDoctorForm/AddDoctorForm.tsx
@@ -4,13 +4,36 @@ import { Link } from 'react-router';
 import { AutoComplete } from 'primereact/autocomplete';
 import { specialityList } from '../../data/specialityList';
 //import { db } from '../../db/db';
-//import type { DoctorDataProp } from '../../db/db';
+import type { DoctorDataProp } from '../../db/db';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 export const AddDoctorForm = () => {
   const [suggestDocSpec, setSuggestDocSpec] = useState<string[]>([]);
   //const current = 0;
+
+  //SPECIALITY autocomplete "našeptávač"
+  const searchSpeciality = (e: { query: string }) => {
+    const query = e.query.toLocaleLowerCase();
+    const suggestion = specialityList.filter((specialist) =>
+      specialist.toLowerCase().startsWith(query),
+    );
+    setSuggestDocSpec(suggestion);
+  };
+
+  //YUP validation
+  const SignupSchema = Yup.object().shape({
+    speciality: Yup.string().required('Povinné'),
+    name: Yup.string(),
+    address: Yup.string(),
+    addressDetail: Yup.string(),
+    phone: Yup.string().matches(
+      /^\+?\d{9,15}$/,
+      'Telefon musí mít 9-15 číslic a může začínat +',
+    ),
+    email: Yup.string().email('Neplatný e-mail'),
+    frequency: Yup.string().required('Povinné'),
+  });
 
   //Add to database
   /*const addDoctor = async () => {
@@ -41,34 +64,9 @@ export const AddDoctorForm = () => {
     }
   };*/
 
-  //SPECIALITY autocomplete "našeptávač"
-  const searchSpeciality = (e: { query: string }) => {
-    const query = e.query.toLocaleLowerCase();
-
-    const suggestion = specialityList.filter((specialist) =>
-      specialist.toLowerCase().startsWith(query),
-    );
-
-    setSuggestDocSpec(suggestion);
-  };
-
-  //YUP validace
-  const SignupSchema = Yup.object().shape({
-    speciality: Yup.string().required('Povinné'),
-    name: Yup.string(),
-    email: Yup.string().email('Neplatný e-mail'),
-    phone: Yup.string().matches(
-      /^\+?\d{9,15}$/,
-      'Telefon musí mít 9–15 číslic a může začínat +',
-    ),
-    frequency: Yup.string().required('Povinné'),
-  });
-
-  //type FormDataErrorType = Partial<DoctorDataProp>;
-
   return (
     <>
-      <Formik
+      <Formik<DoctorDataProp>
         initialValues={{
           id: 0,
           speciality: '',
@@ -88,9 +86,15 @@ export const AddDoctorForm = () => {
             className="addDoctorForm" /* doplnit onSubmit - vyřeší se problém s validací? Vyzkoušet */
           >
             <label className="addDoctorForm__label">
-              Specialista:
+              <span>
+                Specialista <span className="formRequired">*</span>:
+              </span>
               <AutoComplete
-                className="addDoctor__autoComplete"
+                className={
+                  formik.touched.speciality && formik.errors.speciality
+                    ? 'addDoctor__autoComplete input--error'
+                    : 'addDoctor__autoComplete'
+                }
                 value={formik.values.speciality}
                 suggestions={suggestDocSpec}
                 completeMethod={searchSpeciality}
@@ -98,46 +102,111 @@ export const AddDoctorForm = () => {
                 onBlur={() => formik.setFieldTouched('speciality', true)}
                 required
               />
-              <ErrorMessage name="speciality" component="p" />
+              <ErrorMessage
+                name="speciality"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
             <label className="addDoctorForm__label">
               Jméno:
-              <Field name="name" className="addDoctorForm__input" />
-              <ErrorMessage name="name" component="p" />
+              <Field
+                name="name"
+                className={
+                  formik.touched.name && formik.errors.name
+                    ? 'addDoctorForm__input input--error'
+                    : 'addDoctorForm__input'
+                }
+              />
+              <ErrorMessage
+                name="name"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
-            {formik.errors.name ? <p>{formik.errors.name}</p> : null}
-            <label className="      addDoctorForm__label">
+            <label className="addDoctorForm__label">
               Adresa:
-              <Field name="address" className="addDoctorForm__input" />
-              <ErrorMessage name="address" component="p" />
+              <Field
+                name="address"
+                className={
+                  formik.touched.address && formik.errors.address
+                    ? 'addDoctorForm__input input--error'
+                    : 'addDoctorForm__input'
+                }
+              />
+              <ErrorMessage
+                name="address"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
             <label className="addDoctorForm__label">
               Detail adresy:
-              <Field name="addressDetail" className="addDoctorForm__input" />
-              <ErrorMessage name="addressDetail" component="p" />
+              <Field
+                name="addressDetail"
+                className={
+                  formik.touched.addressDetail && formik.errors.addressDetail
+                    ? 'addDoctorForm__input input--error'
+                    : 'addDoctorForm__input'
+                }
+              />
+              <ErrorMessage
+                name="addressDetail"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
             <label className="addDoctorForm__label">
               Telefon:
-              <Field name="phone" className="addDoctorForm__input" />
-              <ErrorMessage name="phone" component="p" />
+              <Field
+                name="phone"
+                className={
+                  formik.touched.phone && formik.errors.phone
+                    ? 'addDoctorForm__input input--error'
+                    : 'addDoctorForm__input'
+                }
+              />
+              <ErrorMessage
+                name="phone"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
             <label className="addDoctorForm__label">
               E-mail:
               <Field
                 name="email"
                 type="email"
-                className="addDoctorForm__input"
+                className={
+                  formik.touched.email && formik.errors.email
+                    ? 'addDoctorForm__input input--error'
+                    : 'addDoctorForm__input'
+                }
               />
-              <ErrorMessage name="email" component="p" />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
             <label className="addDoctorForm__label">
-              Pravidelnost prohlídek:
+              <span>
+                Pravidelnost prohlídek <span className="formRequired">*</span>:
+              </span>
               <Field
                 name="frequency"
-                className="addDoctorForm__input"
+                className={
+                  formik.touched.frequency && formik.errors.frequency
+                    ? 'addDoctorForm__input input--error'
+                    : 'addDoctorForm__input'
+                }
                 required
               />
-              <ErrorMessage name="frequency" component="p" />
+              <ErrorMessage
+                name="frequency"
+                component="p"
+                className="addDoctorForm__errorMessage"
+              />
             </label>
             <div className="addDoctorForm__buttons ">
               <input
--- src/pages/AddDoctorPage/AddDoctorPage.tsx
+++ src/pages/AddDoctorPage/AddDoctorPage.tsx
@@ -5,6 +5,7 @@ export const AddDoctorPage = () => {
   return (
     <>
       <h2>Přidat doktora do seznamu</h2>
+      <p className="form__note">Pole označená * jsou povinná.</p>
       <AddDoctorForm />
     </>
   );
