0085a4ac71fe7312dcca6cb97d11a0bf88248e91
Linda Hrdinová <hrlin@seznam.cz>
2025-11-25 19:01:13 +0100

    Next appointement  - fake data (better)

--- src/components/NextAppointment/NextAppointment.tsx
+++ src/components/NextAppointment/NextAppointment.tsx
@@ -4,7 +4,7 @@ import './style.css';
 const appointmentList = [
   { day: 'St', date: '26.11.2025', time: '18:00', doctor: 'Frontentologie' },
   { day: 'Pá', date: '5.12.2025', time: '16:30', doctor: 'Rentgen' },
-  { day: 'Po', date: '27.1.2026', time: '9:00', doctor: 'Endokrinologie' },
+  { day: 'Po', date: '26.1.2026', time: '9:00', doctor: 'Endokrinologie' },
 ];
 
 export const NextAppointment: React.FC = () => {
--- src/pages/AppointmentListPage/AppointmentListPage.tsx
+++ src/pages/AppointmentListPage/AppointmentListPage.tsx
@@ -2,17 +2,29 @@ import { FaPlus } from 'react-icons/fa';
 import { BigButton } from '../../components/BigButton/BigButton';
 import { Appointment } from '../../components/Appointment/Appointment';
 
+const appointmentList = [
+  { day: 'St', date: '26.11.2025', time: '18:00', doctor: 'Frontentologie' },
+  { day: 'Pá', date: '5.12.2025', time: '16:30', doctor: 'Rentgen' },
+  { day: 'Po', date: '26.1.2026', time: '9:00', doctor: 'Endokrinologie' },
+  { day: 'Čt', date: '22.1.2026', time: '7:45', doctor: 'Praktický lékař' },
+];
+
 export const AppointmentList = () => {
   return (
     <>
       <h2>Seznam termínů</h2>
       <p>Tady bude seznam termínů.</p>
-      <Appointment
-        day={'St'}
-        date={'26.11.2025'}
-        time={'18:00'}
-        speciality={'Frontentologie'}
-      />
+      {appointmentList.map((app, id) => {
+        return (
+          <Appointment
+            key={id}
+            day={app.day}
+            date={app.date}
+            time={app.time}
+            speciality={app.doctor}
+          />
+        );
+      })}
       <BigButton
         textButton={<FaPlus />}
         urlButton="new-appointment"
