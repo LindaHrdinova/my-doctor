318295cd616586c17cac089c2e6401e5b7271bd5
Linda Hrdinová <hrlin@seznam.cz>
2025-11-25 18:41:55 +0100

    Create <AddAppointmentForm/>

---
+++ src/components/AddAppointmentForm/AddAppointmentForm.tsx
@@ -0,0 +1,5 @@
+import type React from 'react';
+
+export const AddAppointmentForm: React.FC = () => {
+  return <h3>Formulář</h3>;
+};
--- src/pages/AddAppointmentPage/AddAppointmentPage.tsx
+++ src/pages/AddAppointmentPage/AddAppointmentPage.tsx
@@ -1,3 +1,4 @@
+import { AddAppointmentForm } from '../../components/AddAppointmentForm/AddAppointmentForm';
 import { BigButton } from '../../components/BigButton/BigButton';
 
 export const AddAppointmentPage = () => {
@@ -5,6 +6,7 @@ export const AddAppointmentPage = () => {
     <>
       <h2>Přidat termín</h2>
       <p>Tady bude formulář pro přidání návštěvy.</p>
+      <AddAppointmentForm />
       <BigButton urlButton="../" textButton="Domů" primaryButton={false} />
     </>
   );
