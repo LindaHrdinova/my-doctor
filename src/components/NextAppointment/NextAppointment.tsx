ee4658adffd7769acfda9c4321501a60ca14e5e7
Linda Hrdinová <hrlin@seznam.cz>
2026-01-06 19:06:45 +0100

    Connect 'Next Appoitement' with 'Appointment' db

--- src/components/NextAppointment/NextAppointment.tsx
+++ src/components/NextAppointment/NextAppointment.tsx
@@ -1,26 +1,29 @@
+import { useLiveQuery } from 'dexie-react-hooks';
 import { Appointment } from '../Appointment/Appointment';
 import './style.css';
+import { db } from '../../db/db';
 
-const appointmentList = [
+/*const appointmentList = [
   { day: 'St', date: '26.11.2025', time: '18:00', doctor: 'Frontentologie' },
   { day: 'Pá', date: '5.12.2025', time: '16:30', doctor: 'Rentgen' },
   { day: 'Po', date: '26.1.2026', time: '9:00', doctor: 'Endokrinologie' },
-];
+];*/
 
 export const NextAppointment: React.FC = () => {
+  const appointments = useLiveQuery(() =>
+    db.appointments.orderBy('date').limit(3).toArray(),
+  );
+
   return (
     <>
-      {appointmentList.map((app, id) => {
-        return (
-          <Appointment
-            key={id}
-            day={app.day}
-            date={app.date}
-            time={app.time}
-            speciality={app.doctor}
-          />
-        );
-      })}
+      {appointments?.map((appointment) => (
+        <Appointment
+          key={appointment.id}
+          date={appointment.date}
+          time={appointment.time}
+          speciality={appointment.speciality}
+        />
+      ))}
     </>
   );
 };
